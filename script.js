b3f9b0e0a23e272420954d00748cd97d3fffe6f9
Vkscoma <vkscoma@gmail.com>
2023-06-08 09:52:56 -0700

    Updates to movie quote app

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiKEY = `hMh3HuAr0u4JpmmpQoiKXQ==cec9rwBDYeVWILgg`;
 function generateQuote() {
     const quote = document.querySelector("#quote");
     const author = document.querySelector("#author");
+    const quoteContainer = document.querySelector(".quote");
 
     fetch("https://api.api-ninjas.com/v1/quotes?category=movies", {
         headers: {
@@ -18,4 +19,11 @@ function generateQuote() {
 
 const button = document.querySelector("#new-quote");
 
-button.addEventListener("click", generateQuote);
+button.addEventListener("click", () => {
+    generateQuote();
+    const quoteContainer = document.querySelector(".quote");
+    quoteContainer.classList.add("animate__fadeInUp")
+    setTimeout(() => {
+        quoteContainer.classList.remove("animate__fadeInUp")
+    }, 2500);
+});
